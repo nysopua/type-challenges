dd173a48bbeffbb64fc6da34a8c814d7229db386
nysopua <shu.endo@enechange.co.jp>
2024-12-12 08:43:47 +0900

    easy問題制覇

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,14 @@
-type Includes<T extends readonly any[], U> = any
+// コンパイラは Conditional Types (T extends U ? X : Y) のうち T が型パラメータ単体だった場合は必ず遅延評価する
+
+type IsEqual<T, U> =
+  (<Dummy>() => Dummy extends T ? true : false) extends
+  (<Dummy>() => Dummy extends U ? true : false)
+    ? true
+    : false
+
+type Includes<T extends readonly unknown[], U> =
+  T extends [infer Head, ...infer Tail]
+    ? IsEqual<Head, U> extends true
+      ? true
+      : Includes<Tail, U>
+    : false
