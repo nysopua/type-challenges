631380407c9211d3adf9d34c85ad223676fcbce7
nysopua <shu.endo@enechange.co.jp>
2024-12-16 09:28:21 +0900

    日課

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,10 @@
-type MyReadonly2<T, K> = any
+// type MyReadonly2<T, K extends keyof T = keyof T> = T & {
+//   readonly [P in K]: T[P];
+// }
+// とすると、readonlyでないTのKでないkeyも含むようになってしまう。
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+} & {
+  readonly [P in K]: T[P];
+}
--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,3 @@
-type TupleToUnion<T> = any
+type TupleToUnion<T> = T extends (infer S)[] ? S : never
+
+// type TupleToUnion<T extends any[]> = T[number]でも良い
